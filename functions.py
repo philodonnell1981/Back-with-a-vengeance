6abed4d475a1e95b1cd0ae294d0fbb6d0a7b8cec
Phil ODonnell <podonnell511@aol.com>
2025-03-16 11:18:35 +0000

    Add files via upload
    re-familiarising myself with the basics of python functions eg round, abs, len, bin and hex

---
+++ functions.py
@@ -0,0 +1,11 @@
+print(round(3.14)) # rounds to the nearest integer
+print(round(3.5)) # rounds to 4
+print(round(2.5)) # round to 2, same for input 0.5, but not 1.5!
+print(round(3.14, 1)) #round also accepts a 2nd argument specifying how many decimal places it should round to.
+print(round(7.778899, 3))
+print(abs(25)) # absolute is distance from zero in the positive form
+print(abs(-25))
+print(abs(-3.14))
+print(len('Philippe')) #prints the length of the string.
+print(bin(255)) #converts an integer number to a binary string prefixed with '0b'.
+print(hex(255)) #converts an int to a lowercase hexadecimal string prefixed with '0x'.
