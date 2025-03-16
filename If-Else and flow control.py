18883a4b527b93c71fc33c87b64147f43b9424d9
Phil ODonnell <podonnell511@aol.com>
2025-03-16 11:19:31 +0000

    Add files via upload
    re-familiarising myself with boolean data types and comparison operators

---
+++ If-Else and flow control.py
@@ -0,0 +1,22 @@
+#Boolean date types True and False
+spam = True
+print(spam)
+# compartison operators
+print(42 == 42)
+print(42 == 99)
+print(2 != 3)
+print(2 != 2)
+print('hello' == 'hello')
+print('hello' == 'Hello')
+print('dog' != 'cat')
+print(True == True)
+print(True != False)
+print(42 == 42.0)
+print(42 == '42')
+print(42 < 100)
+print(42 > 100)
+print(42 < 42)
+eggs = 42
+print(eggs <= 42)
+my_age = 43
+print(my_age >= 44)
